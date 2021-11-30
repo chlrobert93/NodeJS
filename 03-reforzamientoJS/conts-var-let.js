28dfee0d4a4ea263334ffe88a426c26871ff9ad3
Eder <chlrobert93@gmail.com>
2021-11-30 17:05:38 -0600

    Const let var

---
+++ 03-reforzamientoJS/conts-var-let.js
@@ -0,0 +1,25 @@
+var nombre = 'Wolverine';
+
+if( true) {
+  var nombre = 'Magneto';
+
+  console.log(nombre);
+}
+
+
+ console.log(nombre);
+
+
+ //const
+
+ const nombre2 = 'Wolverine2';
+
+ if(true) {
+     const nombre2  = 'Magneto';
+     console.log(nombre2);
+ }
+
+ console.log(nombre2);
+
+ //let para reasignar
+ 
