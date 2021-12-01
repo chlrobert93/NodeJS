79e61c3f0f37c6f67b3dc3a26822aa52cc5c775e
Eder <chlrobert93@gmail.com>
2021-11-30 18:24:55 -0600

    Funciones de Flecha

---
+++ 03-reforzamientoJS/flecha.js
@@ -0,0 +1,15 @@
+/* function  sumar( a, b = 10 ) {
+    return a + b;
+} */
+
+
+const sumar = (a, b) => {
+    return a + b;
+}
+
+//const sumar2 = (a, b) => a + b;
+const saludar = () => 'Hola Mundo';
+
+
+console.log( sumar(5, 10) );
+console.log( saludar() );
