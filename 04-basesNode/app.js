ebeb1813a363004d8bcbdfd1a49a1c6aea939693
Eder <chlrobert93@gmail.com>
2021-12-14 14:39:06 -0600

    Yargs

--- 04-basesNode/app.js
+++ 04-basesNode/app.js
@@ -1,14 +1,23 @@
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('yargs').argv;
 
 console.clear();
 
+console.log( process.argv );
+console.log( argv );
+
+console.log('base: yargs', argv.base);
+
+
+
+
 /* console.log(process.argv); */
-const [ , , arg3 = 'base5' ] = process.argv;
+/* const [ , , arg3 = 'base5' ] = process.argv;
 const [ , base = 5 ] = arg3.split('=');
-console.log( base );
+console.log( base ); */
 
 /* const base = 20; */
 
-crearArchivo(base)
+/* crearArchivo(base)
   .then((nombreArchivo) => console.log(nombreArchivo, "creado"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err)); */
